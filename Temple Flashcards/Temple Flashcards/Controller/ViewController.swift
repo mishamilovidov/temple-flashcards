f557ef07fc9120ad2c1714a9f26d652b5ec0357c
Misha Milovidov <mmilovidov@doterra.com>
2017-10-15 13:58:26 -0600

    Add temple photos; add app icon; add logo to lauchscreen storyboard

---
+++ Temple Flashcards/Temple Flashcards/Controller/ViewController.swift
@@ -0,0 +1,9 @@
+//
+//  ViewController.swift
+//  Temple Flashcards
+//
+//  Created by Misha Milovidov on 10/15/17.
+//  Copyright © 2017 Misha Milovidov. All rights reserved.
+//
+
+import Foundation
